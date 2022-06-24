27e9cf517411325270152ce3a4020864a11132d6
Tangwa <mtshihepo@gmail.com>
2022-06-24 15:19:18 +0200

    Add to exsisting repo

--- create.py
+++ create.py
@@ -2,11 +2,24 @@ from carta.session import Session
 from carta.token import BackendToken
 from carta.browser import Chrome
 
+class Create:
+
+    def _init_(self, fitsImage):
+        self.fitsImage = fitsImage
+
+
+
 # New session, connect to an existing backend
-#session = Session.create(Chrome(), "FRONTEND URL", BackendToken("SECURITY TOKEN"))
+    #session = Session.create(Chrome(), "FRONTEND URL", BackendToken("SECURITY TOKEN"))
 
 # New session, start local backend
-session = Session.start_and_create(Chrome())
+    session = Session.start_and_create(Chrome())
 
 # New session, start remote backend
-#session = Session.start_and_create(Chrome(), remote_host="REMOTE HOSTNAME OR IP")
+    #session = Session.start_and_create(Chrome(), remote_host="REMOTE HOSTNAME OR IP")
+
+# Opening image in CARTA
+    img = session.open_image(fitsImage)
+
+#Saving rendered image
+    session.save_rendered_view("rendered.png")
---
+++ objGen.py
@@ -0,0 +1,10 @@
+from create import Create
+
+class ObjGen:
+
+    def _init_(self, img):
+        self.img = img
+
+    def main():
+        img = input("Please enter image path:")
+                    
