8fbd754c8c437a661abce3c2741f278f7cb65cb9
chav0-moe <61418955+chav0-moe@users.noreply.github.com>
2022-08-15 11:14:48 +0200

    Layers class with new test script

---
+++ testScript.py
@@ -0,0 +1,20 @@
+from carta.session import Session
+from layers import Layers
+
+session = Session.interact("http://192.168.0.191:3002/?token=B84B9C09-660E-4708-9CF0-08AFB6D121DE", 3056068301)
+
+img = session.open_image("/Users/tangwashihepo/Desktop/VecLib/WFPC2u5780205r_c0fx.fits.txt")
+
+layerLists = Layers()
+
+layers = layerLists.from_carta(session)
+
+#print(layers.rasterList)
+#print(layers.vectorList)
+#print(layers.originalColor)
+
+
+
+
+
+
