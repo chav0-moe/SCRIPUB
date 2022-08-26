d05d6d0e444f20c6a4e76464f18ffc3bc1c86b9f
MartinVR77 <105722631+MartinVR77@users.noreply.github.com>
2022-08-26 09:58:05 +0200

    Add files via upload
    Removed some code that was commented out in raw_img. Also tested resizing the image so that the vectorization is more effective, though it didn't seem to help.

--- raw_img.py
+++ raw_img.py
@@ -8,18 +8,12 @@ class raw_img():
         self.bool_trace = bool_trace
         self.img = img
         self.colour = colour
-        #self.path = vect_img.get_path(img)
-    
-    #@classmethod                            
-    #def get_path(self, img):
-        #img_bmp = potrace.Bitmap(img)
-        #img_path = img_bmp.trace()
-        #print('Img traced')
-        #return img_path
         
           
     def get_path(self):
+        #width, height = self.img.size
+        #self.img = self.img.resize((width*3, height*3))
         img_bmp = potrace.Bitmap(self.img)
         img_path = img_bmp.trace()
-        print('Img traced')
+        #print('Img traced')
         self.path = img_path
